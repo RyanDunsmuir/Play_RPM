ee0f3258501c7f19874102f0f176a3c51147c7ef
Ryan Dusmuir <ryan_dunsmuir@kansamples.com>
2021-05-31 17:42:01 +0100

    added controllers

--- app/models/order_song.rb
+++ app/models/order_song.rb
@@ -1,4 +1,7 @@
 class OrderSong < ApplicationRecord
   belongs_to :order
   belongs_to :song
+
+  validates :order, :song, presence: true
+  validates :order, uniqueness: { scope: :song }
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,5 @@
 class Song < ApplicationRecord
   belongs_to :user
+
+  validates :title, :album, :artist, presence: true
 end
