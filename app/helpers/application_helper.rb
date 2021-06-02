3c8cdcd53290435da3855cd38700be5c8d77bf67
Ryan Dusmuir <ryan_dunsmuir@kansamples.com>
2021-06-02 12:05:26 +0100

    added covers and encountered cloudinary 404 errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def render_photo(song)
+    if song.cover_photo.attached?
+      photo_link = url_for(song.cover_photo.attachment)
+      cl_image_tag photo_link, height: 400, width: 400, crop: :fill
+    else
+      cl_image_tag song.cover, height: 400, width: 400, crop: :fill
+    end
+  end
+
+  def render_audio(song)
+    audio_file = url_for(song.file.attachment)
+    audio_tag(audio_file.to_s, autoplay: false, controls: true)
+  end
 end
